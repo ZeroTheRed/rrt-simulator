a748b6b8c037bf256eb616c1caf2c91e5a39277f
Tushar Sairam <67111435+ZeroTheRed@users.noreply.github.com>
2020-11-03 16:21:08 +0530

    Updated rrt.py - Major changes
    -> Updated the algorithm with a major code overhaul to now work exactly like how RRT should work
    -> Randomized the start point, goal point, and obstacle points to show extra robustness

--- rrt.py
+++ rrt.py
@@ -6,14 +6,16 @@ import time
 class RRT:
 
     # class constructor
-    def __init__(self, start, goal, obstacle_list, max_iterations=250):
+    def __init__(self, start, goal, obstacle_list, max_iterations=250, max_distance=15):
         self.start = (start[0], start[1])   # starting point
         self.goal = (goal[0], goal[1])      # destination area
         self.obstacles = obstacle_list      # obstacles
         self.max_iterations = max_iterations    # maximum number of iterations
-        self.path_tree = {}                 # the tree to be built from the nodes
+        self.path_tree = {self.start : -1}                 # the tree to be built from the nodes
         self.iter_count = 0                 # counter to keep track of the number of iterations
         self.goal_found = False             # this flag will be set as 'True' if the goal is found
+        self.max_distance = max_distance    # maximum distance between nodes
+        self.obstacle_hit = False
 
         # initialize display
         pygame.init()
@@ -33,26 +35,18 @@ class RRT:
     # randomly samples a point in space
     def random_sample(self):
         sample_point = (np.random.randint(0, 300), np.random.randint(0, 300))
-        if self.distance(sample_point, self.goal) > 10 and self.iter_count <= self.max_iterations:
-            if self.obstacles is not None:
-                obstacle_collide_check = tuple(pygame.Surface.get_at(sample_point)[:3])
-                if obstacle_collide_check != (0, 0, 0):
-                    return sample_point
-
-            else:
-                return sample_point
-
-        elif self.distance(sample_point, self.goal) < 10:
-            print('===== GOAL FOUND =====')
-            self.goal_found = True
+        if self.iter_count <= self.max_iterations:
+            pygame.draw.circle(self.surface, (0, 255, 0), sample_point, 2)
+            pygame.display.update()
             return sample_point
 
     # returns the euclidean distance between two points
     def distance(self, point_1, point_2):
-        return math.sqrt( ((point_1[0] - point_2[0])**2) + ((point_1[1] - point_2[1])**2) )
+        return math.sqrt(((point_1[0] - point_2[0])**2) + ((point_1[1] - point_2[1])**2))
 
     # chains the valid sample point to tree
     def chain_to_tree(self, point, tree):
+        self.obstacle_hit = False
         min = float('infinity')
         if tree is not True:
             chain_destination = self.start
@@ -61,9 +55,41 @@ class RRT:
             if self.distance(point, node) < min:
                 min = self.distance(point, node)
                 chain_destination = node
-        self.path_tree[point] = chain_destination
-        pygame.draw.line(self.surface, (0, 0, 255), chain_destination, point)
-        pygame.display.update()
+
+        try:
+            slope = (point[1] - float(chain_destination[1])) / (point[0] - float(chain_destination[0]))
+            theta = math.atan(slope)
+            theta_deg = math.degrees(theta)
+            print("Iteration: " + str(self.iter_count) + " | Angle: " + str(theta_deg) + " | Coords: " + str(
+                chain_destination) + " | Coords2: " + str(point))
+            if point[0] > chain_destination[0] and point[1] < chain_destination[1]:
+                new_point = (chain_destination[0] + (self.max_distance * math.cos(theta)),
+                             chain_destination[1] + (self.max_distance * math.sin(theta)))
+            elif point[0] < chain_destination[0] and point[1] < chain_destination[1]:
+                new_point = (chain_destination[0] - (self.max_distance * math.cos(theta)),
+                             chain_destination[1] - (self.max_distance * math.sin(theta)))
+            elif point[0] < chain_destination[0] and point[1] > chain_destination[1]:
+                new_point = (chain_destination[0] - (self.max_distance * math.cos(theta)),
+                             chain_destination[1] - (self.max_distance * math.sin(theta)))
+            else:
+                new_point = (chain_destination[0] + (self.max_distance * math.cos(theta)),
+                             chain_destination[1] + (self.max_distance * math.sin(theta)))
+        except ZeroDivisionError:
+            self.obstacle_hit = True
+
+        if self.distance(new_point, self.goal) > 10:
+            if self.obstacles is not None:
+                for obst in self.obstacles:
+                    if self.distance(new_point, (obst[0], obst[1])) < obst[2]:
+                        self.obstacle_hit = True
+        else:
+            self.goal_found = True
+
+        if self.obstacle_hit == False:
+            self.path_tree[new_point] = chain_destination
+            pygame.draw.line(self.surface, (0, 0, 255), chain_destination, new_point)
+            pygame.display.update()
+            time.sleep(0.001)
 
     # the driver function that performs the search
     def driver(self):
@@ -72,16 +98,52 @@ class RRT:
             self.chain_to_tree(new_point, self.path_tree)
             self.iter_count += 1
             if self.goal_found == True:
+                print("==== GOAL FOUND ====")
                 time.sleep(5)
                 exit()
+def distance(point_1, point_2):
+        return math.sqrt(((point_1[0] - point_2[0])**2) + ((point_1[1] - point_2[1])**2))
+
+def start_goal_obstacle_randomizer():
+    temp_goal_collide = True
+    obstacle_collide = True
+    obstacles = []
+    obstacle_limit = np.random.randint(1, 5)
+    obstacle_count = 0
+    start = (np.random.randint(0, 300), np.random.randint(0, 300))
+    while temp_goal_collide:
+        temp_goal = (np.random.randint(0, 300), np.random.randint(0, 300))
+        if temp_goal != start and distance(start, temp_goal) > 10:
+            goal = temp_goal
+            break
+
+    while obstacle_collide and obstacle_count <= obstacle_limit:
+        temp_obstacle = (np.random.randint(0, 300), np.random.randint(0, 300), np.random.randint(5, 30))
+        if (temp_obstacle[0], temp_obstacle[1]) != start and (temp_obstacle[0], temp_obstacle[1]) != goal and distance((temp_obstacle[0], temp_obstacle[1]), start) > temp_obstacle[2] and distance((temp_obstacle[0], temp_obstacle[1]), goal) > temp_obstacle[2]:
+            obstacles.append(temp_obstacle)
+            obstacle_count += 1
+
+    return (start, goal, obstacles)
 
 def main():
-    start = (50, 250)
-    goal = (250, 50)
-    obstacle_list = None
+    start, goal, obstacles = start_goal_obstacle_randomizer()
+    print(start, goal)
+    print(obstacles)
     max_iterations = 500
-    rrt_object = RRT(start, goal, obstacle_list, max_iterations)
+    rrt_object = RRT(start, goal, obstacles, max_iterations)
     rrt_object.driver()
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+
+
